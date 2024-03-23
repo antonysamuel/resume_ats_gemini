bb0aba21c625f7c489c4d442312c201268ad28dc
Antony Samuel <antonysamuel22@outlook.com>
2024-03-23 09:51:10 +0530

    Implemented and tested function calling to parse resume

--- main.py
+++ main.py
@@ -2,18 +2,39 @@ import os
 from dotenv import load_dotenv
 import google.generativeai as genai
 from langchain_community.document_loaders import PyPDFLoader
+from google.ai import generativelanguage as glm
 
 load_dotenv('.env')
 
 
 genai.configure(api_key= os.environ.get("GOOGLE_API_KEY"))
 
-#define model
-model = genai.GenerativeModel(model_name= 'gemini-1.0-pro')
 
 #define  function template
+resume_template = glm.Tool(
+    function_declarations = [
+        glm.FunctionDeclaration(
+            name = 'resume_parse',
+            description = 'Parse the given resume and output name, email, phone, education, work_experience, skills, and others.',
+            parameters = glm.Schema(
+                type = glm.Type.OBJECT,
+                properties = {
+                    'name' : glm.Schema(type = glm.Type.STRING),
+                    'email' : glm.Schema(type = glm.Type.STRING),
+                    'phone' : glm.Schema(type = glm.Type.STRING),
+                    'education' : glm.Schema(type = glm.Type.STRING),
+                    'work experience' : glm.Schema(type = glm.Type.STRING),
+                    'skills' : glm.Schema(type = glm.Type.STRING),
+                },
+                required = ['name']
+            )
+        )
+    ]
+)
 
 
+#define model
+model = genai.GenerativeModel(model_name= 'gemini-1.0-pro', tools= resume_template)
 
 
 
@@ -36,11 +57,13 @@ prompt = f"""
 
     {pdf_content}
 
-    Summarize the given resume
+    Parse the given resume
 
 
 
 """
-result = model.generate_content(prompt)
 
-print(result.text)
+chat = model.start_chat()
+response = chat.send_message(prompt)
+
+print(response.candidates[0].content.parts[0].function_call)
