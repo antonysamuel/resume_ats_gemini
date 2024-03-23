1cd2793e7b9a2e31e9bd6cb6b6fae710a6154399
Antony Samuel <antonysamuel22@outlook.com>
2024-03-23 09:38:41 +0530

    Implemented and tested pdf loading

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 import google.generativeai as genai
+from langchain_community.document_loaders import PyPDFLoader
 
 load_dotenv('.env')
 
@@ -13,6 +14,33 @@ model = genai.GenerativeModel(model_name= 'gemini-1.0-pro')
 #define  function template
 
 
-result = model.generate_content('Hi Gemini!!')
+
+
+
+
+#Loading pdf 
+def load_pdf(file_path):
+    loader = PyPDFLoader(file_path)
+    pages = loader.load_and_split()
+    page_contents = ' '
+    
+    for page in pages:
+        page_contents += page.page_content 
+
+    return page_contents
+
+
+pdf_content = load_pdf('resumes/Data Engineer.pdf')
+
+prompt = f"""
+
+    {pdf_content}
+
+    Summarize the given resume
+
+
+
+"""
+result = model.generate_content(prompt)
 
 print(result.text)
